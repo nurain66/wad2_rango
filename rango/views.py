c8a3fc112b5215682daa764a4e88dc4471dfc72f
usman <3077380y@student.gla.ac.uk>
2026-02-12 10:38:05 +0000

    Chapter 5 complete: Fixed missing import in views

--- rango/views.py
+++ rango/views.py
@@ -1,11 +1,13 @@
 from django.shortcuts import render
-
-from django.shortcuts import render
-
+from rango.models import Category
 from django.http import HttpResponse
 
 def index(request):
-    context_dict = {'boldmessage': "Crunchy, creamy, cookie, candy, cupcake!"}
+    category_list = Category.objects.order_by('-likes')[:5] 
+    
+    context_dict = {}
+    context_dict['boldmessage'] = 'Crunchy, creamy, cookie, candy, cupcake!' 
+    context_dict['categories'] = category_list
     return render(request, 'rango/index.html', context=context_dict)
 
 def about(request):
