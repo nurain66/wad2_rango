037b95260bf39cf87e33b64de6c5450651e15827
usman <3077380y@student.gla.ac.uk>
2026-02-12 12:32:20 +0000

    Chapter 6 done: slugs, category pages and dynamic linking implemented

--- rango/views.py
+++ rango/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from rango.models import Category
+from rango.models import Category, Page
 from django.http import HttpResponse
 
 def index(request):
@@ -13,3 +13,15 @@ def index(request):
 def about(request):
     return HttpResponse("Rango says here is the about page. <a href='/rango/'>Index</a>")
 
+def show_category(request, category_name_slug):
+    context_dict = {} 
+
+    try: 
+        category = Category.objects.get(slug=category_name_slug) 
+        pages = Page.objects.filter(category=category) 
+        context_dict['pages'] = pages 
+        context_dict['category'] = category 
+    except Category.DoesNotExist: 
+        context_dict['category'] = None 
+        context_dict['pages'] = None
+    return render(request, 'rango/category.html', context=context_dict)
